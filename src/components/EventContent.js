08b97542303953225ec83f76bf28eca0af011a50
benlittlenz <benlittle53@gmail.com>
2021-10-04 10:08:41 +1300

    update scheduler colors

--- src/components/EventContent.js
+++ src/components/EventContent.js
@@ -4,8 +4,13 @@ import { Container } from "./styledComponents";
 
 const EventView = ({ event }) => {
   const color = {
-    "Booked In": "#9CA3AF",
-    "In Progress": "#2563EB",
+    "Booked In": "#005cbe",
+    "In Progress": "#2ab27b",
+    "Awaiting Info": "#ff0000",
+    Completed: "#2ab27b",
+    "In QA": "#631dff",
+    "Signed Off": "#2ab27b",
+    "TE Sign Off": "#2ab27b",
   };
   const eventProp = event.extendedProps;
   // console.log("eventProp", eventProp);
